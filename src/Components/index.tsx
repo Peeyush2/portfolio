b66c834417faf35b304efadede0f710fb821d227
Peeyush Gupta <peeyush654@gmail.com>
2023-05-14 17:04:58 -0500

    added a basic scene

---
+++ src/Components/index.tsx
@@ -0,0 +1,7 @@
+import Floor from "./Floor";
+import LightBulb from "./LightBulb";
+import Box from "./Box";
+import Controls from "./OrbitControls";
+import Draggable from "./Draggable";
+
+export { Floor, LightBulb, Box, Controls, Draggable };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+"use client";
+import { Canvas } from "@react-three/fiber";
+import css from "../styles/Home.module.css";
+import { Floor, Box, LightBulb, Controls, Draggable } from "@/Components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello, I am Peeyush
-    </main>
+    <div className={css.scene}>
+      <Canvas
+        shadows
+        className={css.canvas}
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+        <ambientLight color={"white"} intensity={0.2} />
+        <LightBulb position={[0, 3, 0]} />
+        <Draggable>
+          <Box rotateX={3} rotateY={0.2} />
+        </Draggable>
+        <Controls />
+        <Floor position={[0, -1, 0]} />
+      </Canvas>
+    </div>
   );
 }
